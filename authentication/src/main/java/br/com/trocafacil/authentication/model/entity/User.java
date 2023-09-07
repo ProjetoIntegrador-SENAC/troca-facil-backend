179568204e398d37e5573fea77d16c0500bfbcf5
Ronald Garcia <ronaldag58@gmail.com>
2023-09-07 17:30:00 -0300

    feat: add UserDetails interface for Spring recognize that User Entity is the authentication class

--- authentication/src/main/java/br/com/trocafacil/authentication/model/entity/User.java
+++ authentication/src/main/java/br/com/trocafacil/authentication/model/entity/User.java
@@ -3,6 +3,12 @@ package br.com.trocafacil.authentication.model.entity;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity(name = "User")
 @Table(name = "users")
@@ -11,7 +17,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -23,4 +29,34 @@ public class User {
     @Nonnull
     private String password;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Por enquanto, todos os usuários são do mesmo tipo
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
