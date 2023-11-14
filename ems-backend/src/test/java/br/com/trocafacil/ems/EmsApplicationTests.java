314e1e4917873131fc730bd5da00c53639a33ce3
Ronald Garcia <ronaldag58@gmail.com>
2023-11-14 01:35:28 -0300

    part 1: unit tests

--- ems-backend/src/test/java/br/com/trocafacil/ems/EmsApplicationTests.java
+++ ems-backend/src/test/java/br/com/trocafacil/ems/EmsApplicationTests.java
@@ -1,14 +1,35 @@
 package br.com.trocafacil.ems;
 
+import br.com.trocafacil.ems.domain.model.account.User;
+import br.com.trocafacil.ems.domain.model.token.TokenDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EmsApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private TestRestTemplate restTemplate;
 
+	@Test
+	void loginShouldReturnTokenJWT() throws Exception {
+		User user = new User();
+		user.setLogin("ronald");
+		user.setPassword("1234");
+
+		TokenDto token = this.restTemplate.postForObject("http://localhost:8050/auth/login", user, TokenDto.class);
+
+		System.out.println(token.token());
+
+		// Nesse caso, é para retornar true, pois com o login e senha correto, o token é gerado
+		assert(!token.token().isBlank());
 	}
 
 }
---
+++ ems-backend/src/test/java/br/com/trocafacil/ems/TestConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.trocafacil.ems;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplateBuilder().build();
+    }
+
+    @Bean
+    public TestRestTemplate getTestRestTemplate() {
+        return new TestRestTemplate();
+    }
+}
